50273739173e6d73ffbf3c92f73b3896ac9ac767
vinaypanwar110 <vinaypanwar123s@gmail.com>
2024-07-09 19:25:16 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if(!cart){
+  cart =  
+[
   {
      productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
      quantity:2
@@ -8,6 +12,11 @@ export let cart = [
     quantity:1
   }
 ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
 export function addToCart(productId) {
     let matchItem;
       cart.forEach((item) => {
@@ -23,6 +32,7 @@ export function addToCart(productId) {
           quantity: 1,
         });
       }
+      saveToStorage();
   }
 
   export function removeFromCart(productId) {
@@ -33,4 +43,5 @@ export function addToCart(productId) {
          }
      })
      cart = newCart;
+     saveToStorage();
   }
