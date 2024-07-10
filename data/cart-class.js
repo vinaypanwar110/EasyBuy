b41132ebc7c1a53cb12ff6807a0d0202ac008431
vinaypanwar110 <vinaypanwar123s@gmail.com>
2024-07-11 02:09:06 +0530

    convert products into classes

---
+++ data/cart-class.js
@@ -0,0 +1,81 @@
+class Cart {
+  cartItems;
+  #localStorageKey; // making private
+
+  constructor(localStorageKey) {
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
+  }
+
+  #loadFromStorage() {
+    //methods
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    let matchItem;
+    this.cartItems.forEach((item) => {
+      if (productId == item.productId) {
+        matchItem = item;
+      }
+    });
+    if (matchItem) {
+      matchItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    let newCart = [];
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+    this.cartItems = newCart;
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId == cartItem.productId) {
+        matchItem = cartItem;
+      }
+    });
+    matchItem.deliveryOptionId = deliveryOptionId;
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart('cart-oop');
+
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,4 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-opp.js';
 renderOrderSummary();
 renderPaymentSummary();
